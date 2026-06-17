751c27eb7a39a19adcf5b836b7ab0b3340b551ec
nachos101 <nachostrau17@gmail.com>
2026-06-16 21:40:06 -0300

    2

--- Main.java
+++ Main.java
@@ -1,18 +1,25 @@
 package tp_prog3;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<Paquete> listExample = new ArrayList<>();
-		Lector l = new Lector();
 		Camion camioneta = new Camion(1,"FFD",true,80);
 		Paquete p = new Paquete(1,"SAD",60,true,5);
-		
-		listExample.addAll(l.getPaquetes("Paquete.txt"));
-		
 		System.out.println(camioneta);
 		System.out.println(p);
+		
+		
+		try {
+			List<Paquete> listExample = Lector.getPaquetes("Paquete.txt");
+			System.out.println("\n--- Paquetes leídos ---");
+            listExample.forEach(System.out::println);
+		}
+		catch(IOException e) {
+			System.err.println("Error");
+			e.printStackTrace();
+		}
 	}
 
 }
